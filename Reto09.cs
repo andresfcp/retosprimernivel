4b7d07156457501fff87c9be4c38a945977b367e
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-15 01:27:50 -0500

    Create Reto09.cs

---
+++ Reto09.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace calculodias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias, horas, minutos, segundos;
+            Console.WriteLine("Ingrese un número de días:");
+            dias=int.Parse(Console.ReadLine());
+            horas=dias*24;
+            minutos=horas*60;
+            segundos=minutos*60;
+             
+            Console.WriteLine(dias + " días tiene "+horas+ " horas, o " + minutos +" minutos, o "+ segundos+ " segundos.");
+        }
+    }
+}
