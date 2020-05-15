1677d8ab98bcbbdb73fff40a1a3b5456b72818ee
andresfcp <65383172+andresfcp@users.noreply.github.com>
2020-05-14 23:06:47 -0500

    Create Reto01.cs

---
+++ Reto01.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace hola_nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Digita tu nombre");
+            String nombre;
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola,  " + nombre);
+        }
+    }
+}
