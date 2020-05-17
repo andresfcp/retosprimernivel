f642d5bb8fb29408e67a76bd772b9107960fa563
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 21:49:06 -0500

    Update Reto03.cs

--- Reto03.cs
+++ Reto03.cs
@@ -1,3 +1,20 @@
+/*
+Instrucciones: seguro has visto que en Platzi hay más de 600 cursos 
+¿puedes mostrar a que categorías corresponden en una sola línea de código?
+
+
+Debe mostrarse así:
+
+Platzi cuenta con cursos de:
+[categoría1]
+[categoría2]
+[categoría3]
+[categoría4]
+[categoría5]
+[categoría6]
+*/
+
+
 using System;
 using System.Collections.Generic;
 
