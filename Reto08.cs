8ff91648c965e804950e623945e2b980c124deee
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:15:21 -0500

    Update Reto08.cs

--- Reto08.cs
+++ Reto08.cs
@@ -1,3 +1,9 @@
+/*
+Instrucciones: vas con tus amigos a tu restaurante favorito y acuerdan dividir la cuenta. 
+Para facilitar las cosa pedirás al usuario que indique el total a pagar, entre cuantas 
+personas se dvidirá la cuenta, porcentaje de propina a incluir, un porcentaje de impuestos, 
+total a pagar incluyendo propina más impuestos y el total a pagar por cada persona.
+*/
 using System;
 
 namespace splitbill
