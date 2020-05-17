35021233b58e411c38ec6be75905ca6b5075f6d0
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:08:16 -0500

    Update Reto05.cs

--- Reto05.cs
+++ Reto05.cs
@@ -1,3 +1,11 @@
+/*
+Instrucciones: añadiendo un extra al reto anterior ahora el usuario ingresará 3 números, sumarás los 2 primeros y el resultado será multiplicado por el tercero. Añade las consideraciones del punto decimal del reto anterior.
+Ejemplo:
+
+Datos de entrada:2, 3, 4
+Resultado:20
+
+*/
 using System;
 
 namespace reto05
