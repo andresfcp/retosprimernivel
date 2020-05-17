a88190705fb2c3e313ce21d5cf87a3b8882dce24
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:09:55 -0500

    Update Reto06.cs

--- Reto06.cs
+++ Reto06.cs
@@ -1,3 +1,10 @@
+/*
+Instrucciones: llegaste a una fiesta con X cantidad de rebanadas de pizza 
+(indicadas por el usuario), después de un rato se consumió Y cantidad de 
+rebanadas y quedan Z. Fácil ¿cierto?
+El reto está en que expreses lo que sucede es una forma comprensible para 
+cualquier persona, imprescindible pensar en tus usuarios 😉
+*/
 using System;
 
 namespace pizza
