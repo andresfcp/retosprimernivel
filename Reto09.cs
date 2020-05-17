59d488a4caa0824f9e17776a9ab990dbc05e1ca3
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:18:27 -0500

    Update Reto09.cs

--- Reto09.cs
+++ Reto09.cs
@@ -1,3 +1,7 @@
+/*
+Instrucciones: escribe un programa al que le indiques una cantidad de días y como 
+resultado deberá mostrar cuantas horas, minutos y segundos hay en dicha cantidad de días.
+*/
 using System;
 
 namespace calculodias
