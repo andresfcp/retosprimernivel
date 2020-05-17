6b509b127b988373b327a741d4c6e046df792fff
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:20:55 -0500

    Update Reto11.cs

--- Reto11.cs
+++ Reto11.cs
@@ -1,3 +1,6 @@
+/*Instrucciones: pide al usuario ingresar un número mayor a 1000 y otro menor a 100. Indica de una forma 
+sencilla de entender al usuario cuantas veces cabe el número menor a 100 en el número mayor a 1000
+*/
 using System;
 
 namespace reto11
