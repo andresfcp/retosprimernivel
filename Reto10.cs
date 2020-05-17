77df47ec4861d935e7b9e63ff84d69d3ef0eb6bf
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:19:37 -0500

    Update Reto10.cs

--- Reto10.cs
+++ Reto10.cs
@@ -1,3 +1,7 @@
+/*
+Instrucciones: hay 1.609344 km en una milla (mi). Escribe un programa en el que el usuario indique 
+una cantidad de millas y muestre en pantalla el resultado convertido a kilómetros.
+ */
 using System;
 
 namespace millasakilometros
