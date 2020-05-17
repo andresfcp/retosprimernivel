92f7a6996c6db732d7592b84e6db04a8355e4b91
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 21:47:56 -0500

    Update Reto02.cs

--- Reto02.cs
+++ Reto02.cs
@@ -1,3 +1,8 @@
+/*
+Instrucciones: Ahora que sabemos incluir nombres, podemos agregar más datos. 
+Intentemos con un apellido para tener algo así: ``Hola, [nombre] [apellido]```
+*/
+
 using System;
 
 namespace holanombreapellido
