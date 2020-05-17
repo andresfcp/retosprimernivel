26f077821d5fe17620083e1f4c35c3c7617a51de
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 21:50:52 -0500

    Update Reto04.cs

--- Reto04.cs
+++ Reto04.cs
@@ -1,3 +1,13 @@
+/*
+
+Instrucciones: otro clásico conocido, donde pedirás al usuario que ingrese
+2 números y muestre en pantalla el resultado. Si quieres añadir más dificultad 
+puedes utilizar números con punto decimal y especificar el número de decimales
+después del punto.
+
+Ejemplo: 2.5633 + 5.6883 = 8.25
+
+*/
 using System;
 
 namespace suma
