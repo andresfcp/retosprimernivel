2f3ece485c0198ca444b722f85b5dae4ca25076d
Andrés Cortés <65383172+andresfcp@users.noreply.github.com>
2020-05-16 22:12:50 -0500

    Update Reto07.cs

--- Reto07.cs
+++ Reto07.cs
@@ -1,3 +1,9 @@
+/*
+Instrucciones: pide al usuario que indique su nombre y su edad. Como mensaje de salida 
+le indicarás que edad tuvo el año pasado y cuantos años tendrá el siguiente año.
+
+Ejemplo: [nombre] el año pasado tenías X años y el próximo año cumplirás Y años.
+*/
 using System;
 
 namespace edad
